e41061c7735fd5bab85ec133a1a2ae548ba239df
LinggarR01 <riza.linggar@gmail.com>
2024-12-23 23:35:47 +0700

    Making a search input

--- src/app/search/page.js
+++
@@ -1,9 +0,0 @@
-export default function Page(options) {
-    return (
-        <div>
-            <h1>Hello, Next.js!</h1>
-            <p>This is a sample Next.js page.</p>
-            <h2>Hello, Next</h2>
-        </div>
-    )
-}
